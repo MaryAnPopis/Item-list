d5d34394dea94f528b6f690bf6ee08d6a8bb4161
Mariam Domínguez <maryanpopis@hotmail.com>
2017-11-09 00:52:58 -0600

    filter function
    function that filter through the array of items

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 let form = document.getElementById('addForm');
 let itemList = document.getElementById('items');
+let filter = document.getElementById('filter');
 
 // Form submit event
 form.addEventListener('submit', addItem);
 // Delete event
 itemList.addEventListener('click', removeItem);
+// Filter evento
+filter.addEventListener('keyup', filterItems);
 
 function addItem(e) {
     e.preventDefault();
@@ -33,11 +36,30 @@ function addItem(e) {
 }
 
 // Remove item
-function removeItem(e){
-    if(e.target.classList.contains('delete')){
-        if(confirm("Are you sure?")){
+function removeItem(e) {
+    if (e.target.classList.contains('delete')) {
+        if (confirm("Are you sure?")) {
             let li = e.target.parentElement;
             itemList.removeChild(li);
         }
     }
+}
+
+// Filter items
+function filterItems(e) {
+    // Convert to loweCase
+    let text = e.target.value.toLowerCase();
+    // Get the li's
+    let items = itemList.getElementsByTagName('li');
+    // Convert array
+    Array.from(items).forEach(function(item){
+        // Get the item name
+        let itemName = item.firstChild.textContent;
+        if(itemName.toLocaleLowerCase().indexOf(text) != -1 ){
+            item.style.display = 'block';
+        }else{
+            item.style.display = 'none';
+        }
+    });
+
 }
